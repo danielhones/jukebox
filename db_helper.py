d36c62bc1c4a08ce68d702c475291a5a9530d853
Daniel Hones <danielhones@gmail.com>
2017-03-28 00:00:15 -0400

    switched from sqlalchemy to peewee

--- db_helper.py
+++ db_helper.py
@@ -10,56 +10,63 @@ import shutil
 import sys
 import xml.etree.cElementTree as ET
 
+from peewee import DoesNotExist
 from pyItunes import Library
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-from jukebox.models import Album, Artist, DB_FILE, Song
+from jukebox.models import Album, Artist, create_db, DB_FILE, Song
 
 
 ITUNES_FILE = 'itunes_library.xml'
 ITUNES_FILE_BAK = 'itunes_library.xml.bak'
-Base = declarative_base()
 
 
 class SongDb():
     def __init__(self, lib_file=ITUNES_FILE, echo=True):
-        self.engine = create_engine('sqlite:///' + DB_FILE, echo=echo)
         self.library_file = lib_file
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
-
-    def create_db(self):
-        Base.metadata.create_all(self.engine)
 
     def populate_db(self):
         print('Backing up', ITUNES_FILE)
         shutil.copyfile(ITUNES_FILE, ITUNES_FILE_BAK)
+
         print('Parsing iTunes file...')
         library = Library(ITUNES_FILE)
         print('Done, library contains %s songs.' % len(library.songs))
 
         print('Populating db...')
         for key, s in library.songs.items():
-            new_song = Song(id=key, title=s.name, location=s.location)
-            if new_song.location.endswith('.ipa'):
-                continue  # Don't include apps
+            try:
+                if s.location.endswith('.ipa'):
+                    continue  # Don't include apps
 
-            print('Adding', new_song.title)
-            # make album/artist here
-            #q = self.session.query(Artist, name=s.artist)
+                try:
+                    artist = Artist.get(Artist.name == s.artist)
+                except DoesNotExist:
+                    artist = Artist.create(name=s.artist)
+
+                try:
+                    album = Album.get(Album.title == s.album)
+                except DoesNotExist:
+                    album = Album.create(title=s.album, artist=artist)
+
+                new_song = Song.create(title=s.name,
+                                       location=s.location,
+                                       length=s.length,
+                                       artist=artist,
+                                       album=album)
+
+                print('Adding', new_song.title)
+                new_song.save()
+            except Exception as e:
+                print("Error saving song:", e)
             
-            self.session.add(new_song)
-        self.session.commit()
         print('Done')
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == 'update':
-        db = SongDb()
-        db.populate_db()
+        database = SongDb()
+        database.populate_db()
     elif len(sys.argv) == 2 and sys.argv[1] == 'init':
-        db = SongDb()
-        db.create_db()
-        db.populate_db()
+        create_db()
+        database = SongDb()
+        database.populate_db()
--- jukebox/models.py
+++ jukebox/models.py
@@ -1,54 +1,40 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from peewee import *
 
 
 DB_FILE = 'itunesdb.sqlite3'
-ENGINE_ECHO = False
-
-engine = create_engine('sqlite:///' + DB_FILE, echo=ENGINE_ECHO)
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
+db = SqliteDatabase(DB_FILE)
 
 
-class Album(Base):
-    __tablename__ = 'albums'
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    artist_id = Column(Integer, ForeignKey('artist.id'))
-    artist = relationship("Artist", back_populates="albums")
-    songs = relationship("Song", back_populates="album")
+class BaseModel(Model):
+    class Meta:
+        database = db
 
     def __repr__(self):
-        return "Album(id={id}, title='{}', artist_id={})".format(self.title, self.artist_id)
+        return "{name}({data})".format(
+            name=self.__class__.__name__,
+            data=", ".join(['{}={}'.format(k, repr(v)) for k,v in self._data.items()]))
 
 
-class Artist(Base):
-    __tablename__ = 'artists'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    albums = relationship("Album", back_populates="artist")
-
-    def __repr__(self):
-        return "Artist(id={id}, name='{}')".format(self.name)
+class Artist(BaseModel):
+    name = CharField()
 
 
-class Song(Base):
+class Album(BaseModel):
+    title = CharField()
+    artist = ForeignKeyField(Artist, related_name='albums', null=True)
+
+
+class Song(BaseModel):
     """
     Row representing useful info about a song in iTunes library
     """
-    __tablename__ = 'songs'
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    album_id = Column(Integer, ForeignKey('album.id'))
-    album = relationship("Album", back_populates="songs")
-    location = Column(String)
+    title = CharField()
+    location = CharField()
+    length = IntegerField()
+    album = ForeignKeyField(Album, related_name='songs', null=True)
 
-    @property
-    def artist(self):
-        return self.album.artist
-    
-    def __repr__(self):
-        return "Song(id={id}, title='{title}', album_id='{album_id}', location='{location')".format(**self.__dict__)
+
+def create_db():
+    db.connect()
+    db.create_tables([Album, Artist, Song])
+
